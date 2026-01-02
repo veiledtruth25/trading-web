68772a30cd39d395b3b928b42fbe924efaa5de88
Abimanyu <abimanyu25121991@gmail.com>
2026-01-02 13:53:46 +0700

    feat: configure data source URL for veiledtruth25 account

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 // Ganti 'USERNAME' dengan username GitHub kamu
 const CONFIG = {
     // URL ke raw file di repository trading-data
-    DATA_URL: 'https://raw.githubusercontent.com/USERNAME/trading-data/main/accounts/account.json',
+    DATA_URL: 'https://raw.githubusercontent.com/veiledtruth25/trading-data/main/accounts/account.json',
 
     // Interval refresh dalam milidetik (5 menit = 300000)
     REFRESH_INTERVAL: 300000,
